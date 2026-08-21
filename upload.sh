42a88c55cc2128c526073c961a4bddaa45a36af3
Sakamoto9016 <rassassin95@gmail.com>
2026-08-21 21:57:06 +0700

    Update Modev

--- upload.sh
+++ upload.sh
@@ -1,9 +1,5 @@
 cd ~/Documents/Modev/main
 
-git init
 git add .
-git commit -m "Initial commit"
-
-git branch -M main
-git remote add origin https://github.com/Sakamoto9016/Modev.git
-git push -u origin main
+git commit -m "Update Modev"
+git push origin main
